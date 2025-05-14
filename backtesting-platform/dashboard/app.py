65262f137c144d8c91bd9c72a61a6e9e935782b7
QLeBot <lecomteq.ql@gmail.com>
2025-05-14 12:07:23 +0200

    some fix to backtesting-platform

--- backtesting-platform/dashboard/app.py
+++ backtesting-platform/dashboard/app.py
@@ -1,9 +1,15 @@
+#from  sqlite4  import  SQLite4
 import sqlite3
 import pandas as pd
 import streamlit as st
+import os
 
-conn = sqlite3.connect("data/backtest.db")
-df = pd.read_sql("SELECT * FROM trades", conn)
+DB_PATH = os.path.join("data", "backtest.db")
+print(DB_PATH)
+
+con = sqlite3.connect(DB_PATH)
+
+df = pd.read_sql("SELECT * FROM trades", con)
 st.title("Backtest Dashboard")
 st.line_chart(df.set_index("Date")["position"])
 st.write(df.tail())
--- backtesting-platform/engine/backtest.py
+++ backtesting-platform/engine/backtest.py
@@ -2,7 +2,18 @@ import pandas as pd
 from engine.trade_logger import log_trades
 from engine.metrics import calculate_metrics
 
-def run_backtest(ticker, strategy_fn):
+from backtesting import Backtest
+from strategies.rsi_macd_bt import RSIMACDStrategy
+
+def run_backtest(ticker):
+    df = pd.read_csv(f"data/prices/{ticker}.csv", index_col=0, parse_dates=True)
+    df.columns = [col.lower() for col in df.columns]  # ensure lowercase
+    bt = Backtest(df, RSIMACDStrategy, cash=10_000, commission=0.002)
+    stats = bt.run()
+    bt.plot()
+    return stats
+
+def run_custom_backtest(ticker, strategy_fn):
     df = pd.read_csv(f"data/prices/{ticker}.csv", index_col=0, parse_dates=True)
     df = strategy_fn(df)
 
@@ -10,18 +21,4 @@ def run_backtest(ticker, strategy_fn):
     metrics = calculate_metrics(df)
     log_trades(ticker, trades)
     return metrics
-
-# USING BACKTESTING LIBRARY
-# engine/backtest.py
-#import pandas as pd
-#from backtesting import Backtest
-#from strategies.rsi_macd_bt import RSIMACDStrategy
-#
-#def run_backtest(ticker):
-#    df = pd.read_csv(f"data/prices/{ticker}.csv", index_col=0, parse_dates=True)
-#    df.columns = [col.lower() for col in df.columns]  # ensure lowercase
-#    bt = Backtest(df, RSIMACDStrategy, cash=10_000, commission=0.002)
-#    stats = bt.run()
-#    bt.plot()
-#    return stats
 
