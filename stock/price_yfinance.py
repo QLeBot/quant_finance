dfad5618bf1946d015f8637b57e3edf2763f0eda
QLe <31763623+QLeBot@users.noreply.github.com>
2025-12-16 10:35:33 +0100

    modification for ssl

---
+++ stock/price_yfinance.py
@@ -0,0 +1,17 @@
+import truststore
+truststore.inject_into_ssl()
+
+import os
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+
+import yfinance as yf
+
+tickers = ['NVDA']
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker)
+    data = ticker_yahoo.history()
+    last_quote = data['Close'].iloc[-1]
+    print(ticker, last_quote)
